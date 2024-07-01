3562d5e4572f1259638c565538fada66b598ca5c
ismylsmylv <ismayilovsmyl2020@gmail.com>
2024-07-01 17:46:07 +0400

    navbar added

---
+++ src/components/navbar/page.tsx
@@ -0,0 +1,67 @@
+"use client";
+import React, { useState } from "react";
+import "./style.scss";
+import { IoIosNotifications } from "react-icons/io";
+import { MdAccountCircle } from "react-icons/md";
+type Props = {};
+
+function Navbar({}: Props) {
+  const [active, setactive] = useState("left");
+  return (
+    <div className="Navbar">
+      <div className="logo">sharer</div>
+      <div className="navs">
+        <div className="highlighted">
+          <nav>
+            <button
+              className={active == "left" && "active"}
+              onClick={() => {
+                setactive("left");
+              }}
+            >
+              featured
+            </button>
+          </nav>
+          <nav>
+            <button
+              className={active == "right" && "active"}
+              onClick={() => {
+                setactive("right");
+              }}
+            >
+              top
+            </button>
+          </nav>
+        </div>
+        <nav>
+          <button>my apps</button>
+        </nav>
+        <nav>
+          <button>settings</button>
+        </nav>
+      </div>
+      <div className="controls">
+        <div className="group">
+          <svg className="icon" aria-hidden="true" viewBox="0 0 24 24">
+            <g>
+              <path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"></path>
+            </g>
+          </svg>
+          <input placeholder="Search" type="search" className="input" />
+        </div>
+        <nav>
+          <button>
+            <IoIosNotifications color="acafc1" size={30} />
+          </button>
+        </nav>
+        <nav>
+          <button>
+            <MdAccountCircle color="acafc1" size={30} />
+          </button>
+        </nav>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
