57290e8338d651d5657d9f8212db89378b448cb4
ismylsmylv <ismayilovsmyl2020@gmail.com>
2024-07-03 10:55:19 +0400

    inputs added, pending fixes

---
+++ src/app/(pages)/add/page.tsx
@@ -0,0 +1,143 @@
+"use client";
+
+import React from "react";
+import { Formik, Field, Form, ErrorMessage, FieldArray } from "formik";
+
+type Props = {};
+
+const fields = [
+  { name: "name", type: "text" },
+  { name: "price", type: "number" },
+  { name: "info", type: "text" },
+  { name: "releaseDate", type: "text" },
+  { name: "size", type: "number" },
+  { name: "supportContact", type: "text" },
+  { name: "trailer", type: "text" },
+  { name: "updateDate", type: "text" },
+  { name: "email", type: "text" },
+  { name: "inAppPurchases", type: "text" },
+  { name: "description", type: "text" },
+  { name: "cover", type: "text" },
+  { name: "downloads", type: "text" },
+];
+
+function Add({}: Props) {
+  return (
+    <Formik
+      initialValues={{
+        name: "",
+        price: 0,
+        info: "",
+        releaseDate: "",
+        size: 0,
+        supportContact: "",
+        trailer: "",
+        updateDate: "",
+        email: "",
+        inAppPurchases: "",
+        description: "",
+        cover: "",
+        downloads: "",
+        screenshots: [""],
+        category: [""],
+      }}
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          console.log(values);
+          setSubmitting(false);
+        }, 400);
+      }}
+    >
+      {({ values }) => (
+        <Form className="flex flex-col">
+          {fields.map((field) => (
+            <div className="my-1" key={field.name}>
+              <label htmlFor={field.name} className="mr-1">
+                {field.name}
+              </label>
+              <Field name={field.name} type={field.type} className="border-2" />
+              <ErrorMessage name={field.name} component="div" />
+            </div>
+          ))}
+          <label htmlFor="screenshots" className="mr-1">
+            screenshots
+          </label>
+          <FieldArray
+            name="screenshots"
+            render={(arrayHelpers) => (
+              <div>
+                {values.screenshots && values.screenshots.length > 0 ? (
+                  values.screenshots.map((screenshot, index) => (
+                    <div key={index}>
+                      <Field
+                        name={`screenshots.${index}`}
+                        className="border-2 mb-1"
+                      />
+                      <button
+                        type="button"
+                        onClick={() => arrayHelpers.remove(index)}
+                      >
+                        -
+                      </button>
+                      <button
+                        type="button"
+                        onClick={() => arrayHelpers.insert(index, "")}
+                      >
+                        +
+                      </button>
+                    </div>
+                  ))
+                ) : (
+                  <button type="button" onClick={() => arrayHelpers.push("")}>
+                    Add a screenshot
+                  </button>
+                )}
+              </div>
+            )}
+          />
+          <label htmlFor="category" className="mr-1">
+            category
+          </label>
+          <FieldArray
+            name="category"
+            render={(arrayHelpers) => (
+              <div>
+                {values.category && values.category.length > 0 ? (
+                  values.category.map((cat, index) => (
+                    <div key={index}>
+                      <Field
+                        name={`category.${index}`}
+                        className="border-2 mb-1"
+                      />
+                      <button
+                        type="button"
+                        onClick={() => arrayHelpers.remove(index)}
+                      >
+                        -
+                      </button>
+                      <button
+                        type="button"
+                        onClick={() => arrayHelpers.insert(index, "")}
+                      >
+                        +
+                      </button>
+                    </div>
+                  ))
+                ) : (
+                  <button type="button" onClick={() => arrayHelpers.push("")}>
+                    Add a category
+                  </button>
+                )}
+              </div>
+            )}
+          />
+          <div>
+            <button type="submit">Submit</button>
+          </div>
+        </Form>
+      )}
+    </Formik>
+  );
+}
+
+export default Add;
--- src/app/(pages)/test/page.tsx
+++ src/app/(pages)/test/page.tsx
@@ -3,6 +3,7 @@ import { getFirestore } from "@firebase/firestore";
 import { collection, getDocs } from "@firebase/firestore";
 import React, { useEffect, useState } from "react";
 import app from "../../../../src/lib/firebase/firebaseConfig";
+import { Formik, Form, Field, FieldArray } from "formik";
 type Props = {};
 function Test({}: Props) {
   const [datas, setdatas] = useState([]);
@@ -31,6 +32,52 @@ function Test({}: Props) {
             </div>
           );
         })}
+      <Formik
+        initialValues={{ friends: ["jared", "ian", "brent"] }}
+        onSubmit={(values) =>
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+          }, 500)
+        }
+        render={({ values }) => (
+          <Form>
+            <FieldArray
+              name="friends"
+              render={(arrayHelpers) => (
+                <div>
+                  {values.friends && values.friends.length > 0 ? (
+                    values.friends.map((friend, index) => (
+                      <div key={index}>
+                        <Field name={`friends.${index}`} />
+                        <button
+                          type="button"
+                          onClick={() => arrayHelpers.remove(index)} // remove a friend from the list
+                        >
+                          -
+                        </button>
+                        <button
+                          type="button"
+                          onClick={() => arrayHelpers.insert(index, "")} // insert an empty string at a position
+                        >
+                          +
+                        </button>
+                      </div>
+                    ))
+                  ) : (
+                    <button type="button" onClick={() => arrayHelpers.push("")}>
+                      {/* show this when user has removed all friends from the list */}
+                      Add a friend
+                    </button>
+                  )}
+                  <div>
+                    <button type="submit">Submit</button>
+                  </div>
+                </div>
+              )}
+            />
+          </Form>
+        )}
+      />
     </div>
   );
 }
