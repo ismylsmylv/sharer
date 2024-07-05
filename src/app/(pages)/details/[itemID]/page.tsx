68f4c274dc50549e172f4bb9a7ab808b23c4686f
ismylsmylv <ismayilovsmyl2020@gmail.com>
2024-07-05 16:34:46 +0400

    detail page fetch by id connected

--- src/app/(pages)/details/[itemID]/page.tsx
+++ src/app/(pages)/details/[itemID]/page.tsx
@@ -1,9 +1,22 @@
-import React from "react";
+"use client";
+import { fetchAppById } from "@/redux/slice";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 type Props = {};
 
 function Details({ params }: { params: { itemID: string } }) {
-  return <div>Details {params.itemID}</div>;
+  const dispatch = useDispatch();
+  const app = useSelector((state) => state.apps.appByID);
+  useEffect(() => {
+    dispatch(fetchAppById(params.itemID));
+    app && console.log(app);
+  }, []);
+  return (
+    <div>
+      Details {params.itemID} {app && JSON.stringify(app)}
+    </div>
+  );
 }
 
 export default Details;
--- src/redux/slice/index.tsx
+++ src/redux/slice/index.tsx
@@ -2,6 +2,8 @@ import app from "@/lib/firebase/firebaseConfig";
 import {
   DocumentData,
   collection,
+  doc,
+  getDoc,
   getDocs,
   getFirestore,
 } from "@firebase/firestore";
@@ -10,11 +12,34 @@ import type { PayloadAction } from "@reduxjs/toolkit";
 
 export interface AppsState {
   apps: object[];
+  appByID: {
+    data?: DocumentData;
+    id?: string;
+  };
 }
 
 const initialState: AppsState = {
   apps: [],
+  appByID: {},
 };
+
+export const fetchAppById = createAsyncThunk(
+  "apps/fetchAppById",
+  async (id) => {
+    const db = getFirestore(app);
+    const docRef = doc(db, "apps", id);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+      return {
+        data: docSnap.data(),
+        id: docSnap.id,
+      };
+    } else {
+      throw new Error("No such document!");
+    }
+  }
+);
 
 // Asynchronous thunk to fetch apps from Firestore
 export const fetchApps = createAsyncThunk("apps/fetchApps", async () => {
@@ -41,6 +66,12 @@ export const appsSlice = createSlice({
       fetchApps.fulfilled,
       (state, action: PayloadAction<DocumentData[]>) => {
         state.apps = action.payload;
+      }
+    );
+    builder.addCase(
+      fetchAppById.fulfilled,
+      (state, action: PayloadAction<{ data: DocumentData; id: string }>) => {
+        state.appByID = action.payload;
       }
     );
   },
