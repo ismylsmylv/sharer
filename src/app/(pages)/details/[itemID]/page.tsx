458d113cf623e4cf2ef2fc084fd139042486a0a0
ismylsmylv <ismayilovsmyl2020@gmail.com>
2024-07-05 21:22:42 +0400

    detail page updates

--- src/app/(pages)/details/[itemID]/page.tsx
+++ src/app/(pages)/details/[itemID]/page.tsx
@@ -2,19 +2,29 @@
 import { fetchAppById } from "@/redux/slice";
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-
-type Props = {};
+import "./style.scss";
 
 function Details({ params }: { params: { itemID: string } }) {
   const dispatch = useDispatch();
   const app = useSelector((state) => state.apps.appByID);
   useEffect(() => {
-    dispatch(fetchAppById(params.itemID));
+    dispatch(fetchAppById(params.itemID) as any);
     app && console.log(app);
   }, []);
   return (
-    <div>
-      Details {params.itemID} {app && JSON.stringify(app)}
+    <div className="Details">
+      {params.itemID} {app && JSON.stringify(app)}
+      <div className="head">
+        <img src={app.data.icon} alt="icon" />
+        <div className="content">
+          <div className="name">{app.data.name}</div>
+          <div className="publisher">{app.data.publisher}</div>
+          <div className="info">
+            <div className="review">{app.data.review}</div>
+            <div className="downloads">{app.data.downloads}</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
